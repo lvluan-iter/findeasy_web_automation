e17f18a6e2fa2ceb7670d6646e1b10f9fbbe780e
31211027594 <luanle.31211027594@st.ueh.edu.vn>
2025-09-10 23:05:47 +0700

    Script test utils and remove some exist test classes

--- src/test/java/tests/Functional/LoginTest.java
+++ src/test/java/tests/Functional/LoginTest.java
@@ -1,56 +1,49 @@
 package tests.Functional;
 
-import base.TestBase;
+import base.BaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 
 import java.time.Duration;
 
-public class LoginTest extends TestBase {
+public class LoginTest extends BaseTest {
     private LoginPage loginPage;
-
-    @BeforeClass
-    public void setUpPage() {
-        loginPage = new LoginPage(driver);
-        driver.get("https://propertyweb.onrender.com/login");
-    }
 
     @Test
     public void loginWithValidCredentials() {
-        loginPage.login("luan123","Luan@0907");
+        loginPage.login("luan123", "Luan@0907");
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
         WebElement form = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/main/div[1]/div/div/div[3]/div[2]/div")));
         Assert.assertTrue(form.isDisplayed());
     }
 
     @Test
     public void loginWithInvalidUsername() {
-        loginPage.login("ola","Luan@0907");
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        loginPage.login("ola", "Luan@0907");
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
         WebElement error = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"login\"]/p")));
-        Assert.assertEquals(error.getText(),"Login failed. Please check your credentials and try again.");
+        Assert.assertEquals(error.getText(), "Login failed. Please check your credentials and try again.");
     }
 
     @Test
     public void loginWithInvalidPassword() {
-        loginPage.login("luan123","Luan@");
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        loginPage.login("luan123", "Luan@");
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
         WebElement error = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"login\"]/p")));
-        Assert.assertEquals(error.getText(),"Login failed. Please check your credentials and try again.");
+        Assert.assertEquals(error.getText(), "Login failed. Please check your credentials and try again.");
     }
 
     @Test
     public void loginWithInvalidCredentials() {
-        loginPage.login("lua3","Luan@");
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        loginPage.login("lua3", "Luan@");
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
         WebElement error = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"login\"]/p")));
-        Assert.assertEquals(error.getText(),"Login failed. Please check your credentials and try again.");
+        Assert.assertEquals(error.getText(), "Login failed. Please check your credentials and try again.");
     }
 }

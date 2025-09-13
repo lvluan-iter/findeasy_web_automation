7c091136593859802ae964fd9349940cb4346468
31211027594 <luanle.31211027594@st.ueh.edu.vn>
2025-09-13 13:44:04 +0700

    Script and use driver factory in exist code

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,34 +1,30 @@
 package base;
 
 import factory.BrowserFactory;
+import factory.DriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utils.ConfigReader;
 
 public class BaseTest {
-    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     @BeforeMethod
     public void setUp() {
         String browser = ConfigReader.getProperty("browser");
         boolean headless = Boolean.parseBoolean(ConfigReader.getProperty("headless"));
 
-        WebDriver webDriver = BrowserFactory.getDriver(browser, headless);
-        driver.set(webDriver);
+        WebDriver driver = BrowserFactory.getDriver(browser, headless);
+        DriverFactory.setDriver(driver);
 
-        getDriver().get(ConfigReader.getProperty("url"));
+        DriverFactory.getDriver().get(ConfigReader.getProperty("url"));
     }
 
     @AfterMethod
     public void tearDown() {
-        if (getDriver() != null) {
-            getDriver().quit();
-            driver.remove();
+        if (DriverFactory.getDriver() != null) {
+            DriverFactory.getDriver().quit();
+            DriverFactory.unload();
         }
-    }
-
-    public static WebDriver getDriver() {
-        return driver.get();
     }
 }

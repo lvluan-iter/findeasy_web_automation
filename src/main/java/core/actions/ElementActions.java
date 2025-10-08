138f88d34e13de763f79fa41c2ede0286542b871
Luan Le <142068838+lvluan-iter@users.noreply.github.com>
2025-10-08 09:06:08 +0700

    Script element action (#12)
    * Config log for automation framework
    
    * Script element actions

---
+++ src/main/java/core/actions/ElementActions.java
@@ -0,0 +1,118 @@
+package core.actions;
+
+import constants.FrameworkConstants;
+import core.logger.LogHelper;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class ElementActions {
+    private WebDriver driver;
+    private WebElement element;
+    private By locator;
+    private String description;
+
+    public ElementActions(WebDriver driver) {
+        this.driver = driver;
+    }
+    
+    public ElementActions setElement(By locator, String description) {
+        this.locator = locator;
+        this.description = description;
+        waitForVisible();
+        this.element = driver.findElement(locator);
+        return this;
+    }
+
+    public ElementActions click() {
+        LogHelper.info("Clicking on: " + description);
+        element.click();
+        return this;
+    }
+
+    public ElementActions doubleClick() {
+        LogHelper.info("Double-clicking on: " + description);
+        new Actions(driver).doubleClick(element).perform();
+        return this;
+    }
+
+    public ElementActions hover() {
+        LogHelper.info("Hovering over: " + description);
+        new Actions(driver).moveToElement(element).perform();
+        return this;
+    }
+
+    public ElementActions scrollIntoView() {
+        LogHelper.info("Scrolling into view: " + description);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+        return this;
+    }
+
+    public ElementActions waitForVisible() {
+        new WebDriverWait(driver, Duration.ofSeconds(FrameworkConstants.WAIT_TIMEOUT))
+                .until(ExpectedConditions.visibilityOfElementLocated(locator));
+        return this;
+    }
+
+    public ElementActions waitForInvisible() {
+        new WebDriverWait(driver, Duration.ofSeconds(FrameworkConstants.WAIT_TIMEOUT))
+                .until(ExpectedConditions.invisibilityOfElementLocated(locator));
+        return this;
+    }
+
+    public ElementActions waitForClickable() {
+        new WebDriverWait(driver, Duration.ofSeconds(FrameworkConstants.WAIT_TIMEOUT))
+                .until(ExpectedConditions.elementToBeClickable(locator));
+        return this;
+    }
+
+    public String getText() {
+        LogHelper.info("Getting text of: " + description);
+        return element.getText().trim();
+    }
+
+    public String getAttribute(String attribute) {
+        LogHelper.info("Getting attribute '" + attribute + "' of: " + description);
+        return element.getAttribute(attribute);
+    }
+
+    public boolean isVisible() {
+        try {
+            return element.isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    public boolean isEnabled() {
+        return element.isEnabled();
+    }
+
+    public ElementActions jsClick() {
+        LogHelper.info("Clicking via JS on: " + description);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+        return this;
+    }
+
+    public ElementActions keyPress(CharSequence key) {
+        LogHelper.info("Pressing key '" + key + "' on: " + description);
+        element.sendKeys(key);
+        return this;
+    }
+
+    public int getCount() {
+        List<WebElement> elements = driver.findElements(locator);
+        LogHelper.info("Getting count of: " + description + " → " + elements.size());
+        return elements.size();
+    }
+
+    public ElementActions mouseClick() {
+        LogHelper.info("Clicking via mouse action on: " + description);
+        new Actions(driver).moveToElement(element).click().perform();
+        return this;
+    }
+}
